fa2de0ced658aee4ebf6abb216806ba78b5822e5
Iryna Maslova <maslovai@hotmail.com>
2017-01-28 11:21:35 -0800

    revised by Scott

--- app.js
+++ app.js
@@ -1,51 +1,53 @@
-var myAnswerOne = "Russia";
+var myAnswerOne = 'RUSSIA';
 var myAnswerTwo = 'KIA';
-var myAnswerThree = "YES";
-var myAnswerFour = "Sammamish";
-var userName = prompt("What's your name?");
+var myAnswerThree = 'YES';
+var myAnswerFour = 'SAMMAMISH';
+var userName = prompt('What\'s your name?');
 
-alert ("Hello " + userName + ' Thanks for visiting my site!');
-var game = prompt("Would you like to play a guessing game? Yes/No");
-if ((game === "y") || (game === "Y") || (game ==="Yes") || (game === "yes")){
-  //Guessing Game
-  var answerOne = prompt("Let's start with an easy question. Where was I born?");
+alert ('Hello ' + userName + ' Thanks for visiting my site!');
+
+//Guessing Game
+var game = prompt('Would you like to play a guessing game? Yes/No');
+if ((game === 'y') || (game === 'Y') || (game ==='Yes') || (game === 'yes') || (game = 'YES')){
+  var answerOne = prompt('Let\'s start with an easy question. Where was I born?').toUpperCase();
   if (answerOne === myAnswerOne){
-    alert("Correct! I was born in Russia");
+    alert('Correct! I was born in Russia');
   }
   else{
-    alert("Ups!.. Wrong. I was born in Russia");
+    alert('Ups!.. Wrong. I was born in Russia');
   }
-  var answerTwo = prompt("What car do I drive");
+  var answerTwo = prompt('What car do I drive?');
   if (answerTwo.toUpperCase() === myAnswerTwo){
-    alert("Correct! I drive a Kia Sportage");
+    alert('Correct! I drive a Kia Sportage');
   }
   else{
-    alert("Ups!.. Wrong. I drive a Kia Sportage");
+    alert('Ups!.. Wrong. I drive a Kia Sportage');
   }
-  var answerThree = prompt("Do I have children?");
+  var answerThree = prompt('Do I have children?');
   if (answerThree.toUpperCase() === myAnswerThree){
-    alert("Correct! I have two kids");
+    alert('Correct! I have two kids');
   }
   else{
-    alert("Ups!.. Wrong. I have two kids");
+    alert('Ups!.. Wrong. I have two kids');
   }
-  var answerFour = prompt("Where do I live now?");
-  if (answerFour === myAnswerFour){
-    alert("Correct! I live in Sammamish");
-  }
-  else{
-    alert("Ups!.. Wrong. I live in Sammamish");
-  }
-  var Cities = ["New York", "London", "Paris", "Rome"];
-  var myCity =prompt("Name one of my favorite cities:");
-  if (Cities.indexOf(myCity)!= -1) {
-    alert("You got it! " + myCity + " is one of my favorite cities");
+  var answerFour = prompt('Where do I live now?');
+  if (answerFour.toUpperCase() === myAnswerFour){
+    alert('Correct! I live in Sammamish');
   }
   else{
-    alert("Not my favorite.  Paris is everyone's safe bet :)");
+    alert('Ups!.. Wrong. I live in Sammamish');
   }
-
+  //using array
+  var cities = ['NEW YORK', 'LONDON', 'PARIS', 'ROME', 'SEATTLE'];
+  var userResponseForCity =prompt('Name one of my favorite cities:').toUpperCase();
+  if (cities.indexOf(userResponseForCity)!== -1) {
+    alert('You got it! ' + userResponseForCity + ' is one of my favorite cities');
+  }
+  else{
+    alert('Not my favorite.  Paris is everyone\'s safe bet :)');
+  }
+  alert('Game\'s over! :)');
 }
 else {
-  alert("Nice meeting you!");
+  alert('Nice meeting you!');
 }
